1dac8092b5780acf6e903e12e4abc23c14c28626
crunchiumo <87742589+crunchiumo@users.noreply.github.com>
2023-03-27 02:38:53 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var cpubutton = document.getElementById("cpuButton");
+cpubutton.addEventListener("click", function() {
+window.location.href = "cpu.html";
+});
+
+var mbbutton = document.getElementById("mbButton");
+mbbutton.addEventListener("click", function() {
+window.location.href = "motherboard.html";
+});
---
+++ js/motherboard.js
@@ -0,0 +1,4 @@
+var mainbutton = document.getElementById("mainButton");
+mainbutton.addEventListener("click", function() {
+window.location.href = "main.html";
+});
