f22785eaacfe9b996956a1e96657f756fc2ecb54
crunchiumo <87742589+crunchiumo@users.noreply.github.com>
2023-04-24 06:19:30 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,3 +12,12 @@ var buildbutton = document.getElementById("buildButton");
 buildbutton.addEventListener("click", function() {
 window.location.href = "motherboard.html";
 });
+
+function handleFirstTab(e) {
+    if (e.key === 'Tab') {
+      document.body.classList.add('user-is-tabbing');
+      window.removeEventListener('keydown', handleFirstTab);
+    }
+  }
+
+  window.addEventListener('keydown', handleFirstTab);
